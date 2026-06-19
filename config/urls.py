8165c8111e881ca09ad53cc35533b25d805df239
a46926518-star <a46926518@gmail.com>
2026-06-19 13:15:07 +0500

    Swagger integratsiya qilindi

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Cambridge API",
+      default_version='v1',
+      description="Cambridge o'quv markazi uchun API xujjatlari",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
