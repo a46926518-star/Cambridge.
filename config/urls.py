c5d558e5989a1370a762c61aa78a2d685f4fddfb
a46926518-star <a46926518@gmail.com>
2026-06-20 15:41:09 +0500

    Sozlamalar to'liq to'g'irlandi

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('admin/')), # Asosiy sahifani admin panelga yo'naltiradi
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
