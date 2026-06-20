ba865b53ca31c2857e0c76a2522f2f784e095077
a46926518-star <a46926518@gmail.com>
2026-06-20 15:08:24 +0500

    urls.py da import xatosi tuzatildi

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
